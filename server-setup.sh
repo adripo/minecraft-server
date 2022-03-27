f0c5de5a440d1ae780752a79c916dc75ec5a72ad
adripo <26493496+adripo@users.noreply.github.com>
2022-03-27 03:48:33 +0200

    fix: cleanup

--- server-setup.sh
+++ server-setup.sh
@@ -5,10 +5,8 @@ curl "${DW_LINK}" --output "${MC_SERVER}_installer.jar"
 
 # Install server
 java -jar "${MC_SERVER}_installer.jar" --installServer
+
 # Cleanup
 rm -rf "${MC_SERVER}_installer.jar"
 rm -rf "${MC_SERVER}_installer.jar.log"
 rm -rf "run.bat"
-
-## Setup JVM args
-#printf -- "\n-Xms2G -Xmx10G" >> user_jvm_args.txt
